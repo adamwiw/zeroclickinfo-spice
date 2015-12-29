10b1e11d1d9d5bc9c575e564d93324a6e328fd51
dlsdfuh <a20152-1@yahoo.com>
2015-12-29 02:55:55 +0000

    Initial Commit

---
+++ lib/DDG/Spice/Translate.pm
@@ -0,0 +1,28 @@
+package DDG::Spice::Translate;
+
+# ABSTRACT: Write an abstract here
+# Start at https://duck.co/duckduckhack/spice_overview if you are new
+# to instant answer development
+
+use DDG::Spice;
+
+# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
+spice is_cached => 1;
+spice proxy_cache_valid => "200 1d"; # defaults to this automatically
+
+spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
+
+# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
+spice to => 'http://example.com/search/$1';
+
+# Triggers - https://duck.co/duckduckhack/spice_triggers
+triggers any => 'triggerWord', 'trigger phrase';
+
+# Handle statement
+handle remainder => sub {
+
+    # Query is in $_...if you need to do something with it before returning
+    return $_;
+};
+
+1;

adb2ff54a5f9bbf188dfac721f5298a3c072ee56
adamwiw <a20152-1@yahoo.com>
2015-12-31 00:38:19 -0500

    Next iteration

--- lib/DDG/Spice/Translate.pm
+++ lib/DDG/Spice/Translate.pm
@@ -1,7 +1,6 @@
 package DDG::Spice::Translate;
 
 # ABSTRACT: Translate using Yandex Translate API
-
 use DDG::Spice;
 
 # Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
@@ -14,6 +13,15 @@ spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callb
 spice to => 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151229T022356Z.84821db488adeeb1.146aec47c8b050d170184c0bb68aee7baf85b1ae&text=$1&lang=$2&callback={{callback}}';
 
 # Triggers - https://duck.co/duckduckhack/spice_triggers
-triggers query_lc => qr/([\s\S]+)in([\s\S]+)/;
+triggers any => qw (in);
+
+# Handle statement
+handle remainder => sub {
+    my @lang = qw(af Afrikaans ar Arabic az Azerbaijani ba Bashkir be Belarusian bg Bulgarian bs Bosnian ca Catalan cs Czech cy Welsh da Danish de German el Greek en English es Spanish et Estonian eu Basque fa Persian fi Finnish fr French ga Irish gl Galician he Hebrew hr Croatian ht Haitian hu Hungarian hy Armenian id Indonesian is Icelandic it Italian ja Japanese ka Georgian kk Kazakh ko Korean ky Kirghiz la Latin lt Lithuanian lv Latvian mg Malagasy mk Macedonian mn Mongolian ms Malay mt Maltese nl Dutch no Norwegian pl Polish pt Portuguese ro Romanian ru Russian sk Slovak sl Slovenian sq Albanian sr Serbian sv Swedish sw Swahili tg Tajik th Thai tl Tagalog tr Turkish tt Tatar uk Ukrainian uz Uzbek vi Vietnamese zh Chinese);
+
+    my @r = $_ =~ /(\w+\s\S+\d\D+)\s\D(\w+)/ig;
+
+    return if grep {$_ eq @r[-1]} @lang;
+};
 
 1;
--- share/spice/translate/translate.js
+++ share/spice/translate/translate.js
@@ -1,24 +1,22 @@
 (function (env) {
     "use strict";
-    env.ddg_spice_ = function(api_result){
+
+    env.ddg_spice_translate = function(api_result){
 
         // Validate the response (customize for your Spice)
         if (!api_result || api_result.error || api_result.text.length === 0) {
-            return Spice.failed('Yandex Translate API failed');
+            return Spice.failed('translate');
         }
-
         // Render the response
         Spice.add({
-            id: "translate",
-
             // Customize these properties
-            name: "Translate",
+            name: 'Translate',
             data: api_result.text,
             meta: {
-                sourceName: "Yandex.Translate",
-                sourceUrl: 'https://translate.yandex.com/?text=' + api_result.text + '&lang=' + api_result.lang,
+                sourceName: 'Yandex.Translate',
+                sourceUrl: 'http://translate.yandex.com/',
 		total: api_result.text,
-		itemType: api_result.lang.replace('-', ' to '),
+		itemType: getlang.replace('-', ' to '),
 		searchTerm: decodeURIComponent(query)
             },
             normalize: function(item) {
@@ -27,15 +25,19 @@
                     title: api_result.title,
                     subtitle: api_result.subtitle,
                     image: api_result.icon
-		    url: 'https://translate.yandex.com/?text=' + api_result.text + '&lang=' + api_result.lang,
+		    url: 'https://translate.yandex.com/?text=' + api_result.text + '&lang=' + getlang,
 		    arrowUrl: DDG.get_asset_path('hacker_news','arrow_up.png')
                 };
             },
             templates: {
-                group: 'text',
+                group: 'info',
                 options: {
                     content: Spice.translate.content,
-                    moreAt: true
+                    moreText: 
+		    {
+			href: 'https://translate.yandex.com/?text=' + api_result.text + '&lang=' + getlang',
+			text: 'Translate.Yandex'
+		    }
                 }
             }
         });

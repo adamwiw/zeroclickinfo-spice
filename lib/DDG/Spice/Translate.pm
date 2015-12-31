cc4296f7a582fa6cf8f84db9396e3a11217d9bee
adamwiw <a20152-1@yahoo.com>
2015-12-31 03:05:31 -0500

    sadfj

--- lib/DDG/Spice/Translate.pm
+++ lib/DDG/Spice/Translate.pm
@@ -10,6 +10,7 @@ spice proxy_cache_valid => "200 1d"; # defaults to this automatically
 spice wrap_jsonp_callback => 0; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
 
 # API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
+spice from => '([^/]+)/([^/]+)';
 spice to => 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20151229T022356Z.84821db488adeeb1.146aec47c8b050d170184c0bb68aee7baf85b1ae&text=$1&lang=$2&callback={{callback}}';
 
 # Triggers - https://duck.co/duckduckhack/spice_triggers
